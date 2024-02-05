cb1997c16bd91e7eaea6d5bb77120ef51c0081cf
vlesse <105312471+vlesse@users.noreply.github.com>
2024-02-05 11:29:02 +0800

    Add files via upload

---
+++ resources/lang/en/api_msg.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    "get_record_successfully" => "Get Record Successfully.",
+    "data_not_found" => "Data Not Found.",
+    "please_enter_required_fields" => "Please Enter Required Fields.",
+    "data_not_save" => "Data Not Save.",
+    "add_successfully" => "Add Successfully.",
+    "update_successfully" => "Update Successfully.",
+    "delete_success" => "Delete Successfully.",
+    "User_registration_sucessfuly" => "User registration sucessfuly.",
+    "login_successfully" => "Login Successfully.",
+    "email_pass_worng" => "Email and Password Worng.",
+    "change_type" => "Change Type.",
+    "User_id_worng" => "User Id Worng.",
+    "update_profile_sucessfuly" => "Update profile sucessfuly.",
+    "please_enter_right_rent_video" => "Please Enter Right Rent Video.",
+    "please_enter_right_package_id" => "Please Enter Right Package Id.",
+    "coupon_id_worng" => "Coupon Code Wrong.",
+    "coupon_expriy" => "Coupon Expriy.",
+    "coupon_already_use" => "Coupon Already Used.",
+    "coupon_not_start" => "Coupon Not Start.",
+
+];
